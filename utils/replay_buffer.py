7044b72fed65f33f193274cd800eb11ec5c921f7
Nazarii Revitskyi <nazarii@uw.edu>
2025-04-22 17:20:27 -0700

    fully checked and commented replay buffer class.

--- utils/replay_buffer.py
+++ utils/replay_buffer.py
@@ -1,15 +1,37 @@
 import random
+from collections import namedtuple, deque
 
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
 class ReplayBuffer:
-    def __init__(self):
-        self.memory = []
+    def __init__(self, capacity):
+        """
+        Replay Buffer defined by named tuples representing a single
+        transition in our environment mapping (state, action) to
+        (next_state, reward)
+        :param capacity: limit of number of transitions in the replay buffer
+        """
+        self.buffer = deque([], maxlen=capacity)
 
-    # transitions are in the form (state, action, new_state, reward, terminated)
-    def append(self, transition):
-        self.memory.append(transition)
+    def push(self, *args):
+        """
+        Push the transition that the agent observes
+        :param args: state, action, next_state, reward
+        """
+        self.buffer.append(Transition(*args))
 
-    def sample(self, sample_size):
-        return random.sample(self.memory, sample_size)
+    def sample(self, batch_size):
+        """
+        Return a list of 'batch_size' length of random transitions chosen
+        from ReplayBuffer.
+        :param batch_size: size of list of random transitions to return
+        :return: random list of transitions of 'batch_size' length
+        """
+        return random.sample(self.buffer, batch_size)
 
     def __len__(self):
-        return len(self.memory)
+        """
+        Returns length of ReplayBuffer
+        :return: number of transitions in ReplayBuffer
+        """
+        return len(self.buffer)
